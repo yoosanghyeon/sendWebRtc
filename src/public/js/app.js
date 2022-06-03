c3c92cddc1c68a8cee5c55d4a9e18526af0fd134
YooSangHyeon <yoosanghyeon1@gmail.com>
2022-06-03 16:37:10 +0900

    func/change-port

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,249 +1,29 @@
 const socket = io();
+socket.connect()
+const image = document.getElementById('image');
 
-const myFace = document.getElementById("myFace");
-const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera");
-const camerasSelect = document.getElementById("cameras");
-const call = document.getElementById("call");
-const otherVideos = document.getElementById("otherVideos");
-const micMenu = document.getElementById("micMenu");
-const micGain = document.getElementById("micGain");
-const gainValue = document.getElementById("gainValue");
-const otherFace = document.getElementById("otherFace");
-const outerTitle = document.getElementById("outerTitle");
-const roomTitle = document.getElementById("roomTitle");
-const changeCodecsMenu = document.getElementById("changeCodecsMenu");
-const videoCodecsSelects = document.getElementById("videoCodecsSelects");
-
-call.hidden = true;
-
-let myStream;
-let muted = false;
-let cameraOff = false;
-let roomName;
-
-
+const firstUser = document.getElementById('firstUser')
+firstUser.innerText = "OTHER"
 let myPeerConnections = {};
-let otherVideoViews = {};
-let myDataChannel;
+let myDataChannels = [];
 
-const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
-  'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
+socket.emit("join_room");
+socket.on('stream', (imageData) =>{
+  image.src = imageData
 
-async function getCameras() {
-
-  try {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const cameras = devices.filter((device) => device.kind === "videoinput");
-    const currentCamera = myStream.getVideoTracks()[0];
-    cameras.forEach((camera) => {
-      const option = document.createElement("option");
-      option.value = camera.deviceId;
-      option.innerText = camera.label;
-      if (currentCamera.label === camera.label) {
-        option.selected = true;
-      }
-      camerasSelect.appendChild(option);
-    });
-  } catch (e) {
-    console.log(e);
-    alert(e)
+  if(firstUser.text !== 'firstUser'){
+    firstUser.innerText = 'firstUser'
   }
 
-}
-
-
-
-
-
-async function getMedia(deviceId) {
-  
-  await getCodecs();
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  
-  // 마이크 체크 
-  var isMic = false;
-  muteBtn.hidden = true;
-  micMenu.hidden = true;
-  const micDevices = devices.filter((device) => device.kind === "audioinput");
+  myDataChannels.forEach((dataChannel) =>{
  
-  if(micDevices.length > 0){
-    isMic = true;
-    muteBtn.hidden = false;
-    micMenu.hidden = false;
-   
-    micDevices.forEach((micDevice) => {
-      console.log(micDevice);
-    });
-    
-  }
-  
-
-  if(myStream){
-    // Video만 변경
-    myStream.getTracks().forEach(track => {
-
-      if(track.kind == "video"){
-        track.stop();
-      }
-    });
-  }
-
-  const initialConstrains = {
-    audio: isMic,
-    video: { 
-      facingMode: "user" ,
-      width : 240,
-      height: 240,
-      frameRate: {
-          min: 7,
-          max: 30
-      }
-    },
-  };
-
-  const cameraConstraints = {
-    audio: isMic,
-    video: { 
-      deviceId: { exact: deviceId },
-      width : 240,
-      height: 240,
-      frameRate: {
-          min: 7,
-          max: 30
-      }
-    }
-  };
-
-  try {
-    myStream = await navigator.mediaDevices.getUserMedia(
-      deviceId ? cameraConstraints : initialConstrains
-    );
-
-    // mic 볼륨 조절
-    if(isMic){
-      gotStream(myStream);
-    }
-  
-      
-    if (!deviceId) {
-      await getCameras();
-    }
-
-    onMutued();
-
-    myFace.srcObject = myStream; 
-    otherFace.srcObject = myStream;
-
-
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// temp sample code 
-function onMutued(){
-  muted = true;
-  muteBtn.innerText = "Unmute";
-  myStream
-  .getAudioTracks()
-  .forEach((track) => (track.enabled = false));
-}
-
-function handleMuteClick() {
-  myStream
-    .getAudioTracks()
-    .forEach((track) => (track.enabled = !track.enabled));
-  if (!muted) {
-    muteBtn.innerText = "Unmute";
-    muted = true;
-  } else {
-    muteBtn.innerText = "Mute";
-    muted = false;
-  }
-}
-
-function handleCameraClick() {
-
-  myStream
-    .getVideoTracks()
-    .forEach((track) => (track.enabled = !track.enabled));
-  if (cameraOff) {
-    cameraBtn.innerText = "Turn Camera Off";
-    cameraOff = false;
-  } else {
-    cameraBtn.innerText = "Turn Camera On";
-    cameraOff = true;
-  }
-
-  onMutued();
-}
-
-async function handleCameraChange() {
-  
-  await getMedia(camerasSelect.value);
-
-   // TODO : AUDIO Track 바뀌는지 확인
-  for(socketId in myPeerConnections){
-    const myPeerConnection = myPeerConnections[socketId];  
-
-    try{
-
-      let videoTrack = myStream.getVideoTracks()[0];
-      var sender = myPeerConnection.getSenders().find(function(s) {
-        return s.track.kind == videoTrack.kind;
-      });
-
-      sender.replaceTrack(videoTrack);
-
-    }catch(e){
-      console.log( "switch camera error : ", e);
+    if(dataChannel.readyState === 'open'){ 
+      dataChannel.send(imageData)
     }
     
+  })
 
-  }
-
-}
-
-
-myFace.addEventListener("click", () =>{
-  otherFace.srcObject = myFace.srcObject;
-});
-
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-camerasSelect.addEventListener("input", handleCameraChange);
-videoCodecsSelects.addEventListener("input", handleCodecsChange);
-micGain.addEventListener("input", (event) =>{
-  
-  changeMicrophoneLevel(micGain.value);
-  gainValue.innerText = micGain.value;
-  
-});
-
-
-const welcome = document.getElementById("welcome");
-const welcomeForm = welcome.querySelector("form");
-
-async function initCall() {
-  welcome.hidden = true;
-  call.hidden = false;
-  await getMedia();
-
-}
-
-async function handleWelcomeSubmit(event) {
-  event.preventDefault();
-  outerTitle.hidden = true;
-  roomTitle.hidden = false;
-  const input = welcomeForm.querySelector("input");
-  await initCall();
-  socket.emit("join_room", input.value);
-  roomName = input.value;
-  input.value = "";
-}
-
-welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+})
 
 // Socket Code
 socket.on("welcome", async (users, socketId) => {
@@ -303,33 +83,10 @@ socket.on("ice", (ice, socketId) => {
 
 });
 
-socket.on("userDisconnect", (socketId) => {
-  console.log( "disconnect ::", socketId);
-  
-  if(otherVideoViews[socketId] !==  undefined){
-
-    if(otherVideoViews[socketId].srcObject.id === otherFace.srcObject.id){
-      otherFace.srcObject = myStream;
-    }
-
-    otherVideos.removeChild(otherVideoViews[socketId]);
-    delete otherVideoViews[socketId]
-    delete myPeerConnections[socketId]
-    console.log(otherVideoViews);
-    console.log(myPeerConnections)
-  }
-
- 
-})
-
-socket.on("connect_error", (error) => {
-  console.log(error);
-});
-
 // RTC Code
 async function makeConnection(socketId) {
  
-  console.log("makeConnection");
+
   if(myPeerConnections[socketId]){
     return
   }
@@ -346,17 +103,6 @@ async function makeConnection(socketId) {
   });
 
   
-  // codec 적용 
-  if(supportsSetCodecPreferences){
-    console.log(codecs[0])
-    const transceiver = await myPeerConnection.getTransceivers().find(t => t.sender && t.sender.track === myStream.getVideoTracks()[0]);
-    if(transceiver){
-      transceiver.setCodecPreferences(codecs);
-      console.log("transceiver :: ", transceiver);
-    }
-
-  }
-
   myPeerConnection.addEventListener("icecandidate", (data) =>{
     socket.emit("ice", data.candidate, socket.id, socketId);
   });
@@ -364,38 +110,51 @@ async function makeConnection(socketId) {
   // 구세대 브라우저의 간의 api 변경으로 인한 분기
   if(myPeerConnection.addTrack !== undefined){
 
-    // otherVideoArray
     myPeerConnection.addEventListener("track", (data) => {
-
       console.log(myPeerConnections);
-      addOtherVideoData(socketId, data.streams[0]);
-
     });
-  }else{
-    
-    myPeerConnection.addEventListener("addstream", (data) => {
-
-      addOtherVideoData(socketId, data.stream);
-    });
+  }else{  
+    myPeerConnection.addEventListener("addstream", (data) => {});
   }
   
   // todo (상태 파악하여 디스커넥트 => socket connection 을 이용한 close 처리로 변경 ) 
   myPeerConnection.addEventListener("onconnectionstatechange", (e) =>{
     console.log(e);
   })
-    
-  
-  myStream
-    .getTracks()
-    .forEach((track) => myPeerConnection.addTrack(track, myStream));
 
-  myDataChannel = myPeerConnection.createDataChannel("chat");
-  myDataChannel.addEventListener("message", (event) => console.log(event.data));
-  console.log("made data channel");
+
+
+
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    console.log("datachannel", event)
+
+    event.channel.addEventListener("message", (message) => {
+      // console.log(message.data)
+      image.src = message.data
+
+    });
+
+
+
+  })
+  
+
+  var dataChannelOptions = {
+    ordered: true // 순서 보장 함
+  }
+  const myDataChannel = myPeerConnection.createDataChannel("chat", dataChannelOptions);
+
+  myDataChannel.addEventListener("open", (event) => {
+    console.log('datachannel open')
+  });
+  myDataChannel.addEventListener("close", (event) => {
+    console.log('datachannel close')
+  });
+
+  myDataChannels.push(myDataChannel)
 
   // https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/restartIce
   myPeerConnection.addEventListener("iceconnectionstatechange", event => {
-    console.log(event)
     if (myPeerConnection.iceConnectionState === "failed") {
       /* possibly reconfigure the connection in some way here */
       /* then request ICE restart */
@@ -412,132 +171,4 @@ async function makeConnection(socketId) {
 function handleIce(data) {
   console.log("sent candidate");
   socket.emit("ice", data.candidate, roomName);
-}
-
-function addOtherVideoData(socketId, dataStream){
-  if(otherVideoViews[socketId]) return;
-  const peerFace = document.createElement("video");
-  peerFace.setAttribute("autoplay", "");
-  peerFace.setAttribute("playsinline", "");
-  peerFace.srcObject = dataStream;
-  peerFace.autoplay = true;
-  peerFace.style.width = "120px";
-  peerFace.style.height = "120px";
-  peerFace.style.maxWidth = "4000px";
-  peerFace.controls = false;
-
-  otherVideos.append(peerFace);
-  otherVideoViews[socketId] = peerFace;
-  peerFace.addEventListener("click", () =>{
-    console.log("click", peerFace.srcObject);
-    otherFace.srcObject = peerFace.srcObject;
-  })
-}
-
-
-let gainNode;
-// mic volume contorol
-function changeMicrophoneLevel(value) {
-    if(value && value >= 0 && value <= 2) {
-        gainNode.gain.value = value;
-    }
-}
-
-
-function gotStream(stream) {
-
-  // Get the videoTracks from the stream.
-  const videoTracks = stream.getVideoTracks();
-
-  /**
-   * Create a new audio context and build a stream source,
-   * stream destination and a gain node. Pass the stream into 
-   * the mediaStreamSource so we can use it in the Web Audio API.
-   */
-  const context = new AudioContext();
-  const mediaStreamSource = context.createMediaStreamSource(stream);
-  const mediaStreamDestination = context.createMediaStreamDestination();
-  gainNode = context.createGain();
-  gainNode.gain.value = 1.0;
-  /**
-   * Connect the stream to the gainNode so that all audio
-   * passes through the gain and can be controlled by it.
-   * Then pass the stream from the gain to the mediaStreamDestination
-   * which can pass it back to the RTC client.
-   */
-  mediaStreamSource.connect(gainNode);
-  gainNode.connect(mediaStreamDestination);
-
-  /**
-   * Change the gain levels on the input selector.
-   */
-  // inputLevelSelector.addEventListener('input', event => {
-  //   gainNode.gain.value = event.target.value;
-  // });
-
-  /**
-   * The mediaStreamDestination.stream outputs a MediaStream object
-   * containing a single AudioMediaStreamTrack. Add the video track
-   * to the new stream to rejoin the video with the controlled audio.
-   */
-  const controlledStream = mediaStreamDestination.stream;
-  for (const videoTrack of videoTracks) {
-    controlledStream.addTrack(videoTrack);
-  }
-
-  /**
-   * Use the stream that went through the gainNode. This
-   * is the same stream but with altered input volume levels.
-   */
-  // localVideo.srcObject = controlledStream;
-  myStream = controlledStream;
-  // peerConnection.addStream(controlledStream);
-  // callButton.disabled = false;
-
-}
-
-var codecs;
-let selectedCodecIndex
-async function getCodecs(){
-
-  // FireFox 지원 안함
-  if(supportsSetCodecPreferences){
-    let capabilities = RTCRtpSender.getCapabilities("video");
-     
-    capabilities.codecs.forEach((codec) =>{
-
-      if (['video/red', 'video/ulpfec', 'video/rtx'].includes(codec.mimeType)) {
-        return;
-      }
-      const option = document.createElement("option");
-      option.value = (codec.mimeType + ' ' + (codec.sdpFmtpLine || '')).trim();
-      option.innerText = option.value;
-   
-      videoCodecsSelects.appendChild(option);
-    });
-
-    codecs = capabilities.codecs;
-    selectedCodecIndex = codecs.findIndex((c) => {
-      console.log(c.mimeType);
-      return c.mimeType === "video/H264";
-    });
-    console.log("codecs index : ", selectedCodecIndex);
-    codecs = codecs.splice(selectedCodecIndex, 1);
-    
-    console.log("codec :", codecs);
-    videoCodecsSelects.selectedIndex = selectedCodecIndex;
-
-  }else{
-    changeCodecsMenu.hidden = true;
-    videoCodecsSelects.hidden = true;
-    videoCodecsSelects.disabled = true;
-  }
-  
-}
-
-async function handleCodecsChange() {
-  
-  console.log(videoCodecsSelects.value);
-  alert("전송 코덱은 아직 바뀌지 않음");
-
 }
--- src/server.js
+++ src/server.js
@@ -6,10 +6,7 @@ import cors from 'cors';
 import fs from "fs";
 
 const app = express();
-
-
-
-const port = 2000;
+const port = 3001;
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -19,13 +16,9 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (_, res) => {
   res.render("home");
-  // res.sendFile(__dirname + "/views/" + "index.html")
+
 });
 
-
-app.get("/count", (req, res) => {
-  res.send(users);
-});
 
 app.get("/*", (_, res) => res.redirect("/"));
 
@@ -35,15 +28,42 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer, {
   pingTimeout: 2000,
   pingInterval: 2000,
+  cors: {
+    origin: '*',
+  }
 });
 
 let users = {};
 
 
-wsServer.on("connection", (socket) => {
-  
-  socket.on("join_room", (roomName) => {
 
+wsServer.on("connection", (socket) => {
+    console.log('connection run ')
+
+
+  socket.on('video', (videoStream) =>{
+
+    
+    var messagelength = videoStream.toString().length;
+    console.log('messagelength', messagelength)
+    if (JSON.stringify(videoStream).length > 60000) {
+      
+      return;
+    }
+    // 0.1초당 20kb webrtc 전송 예정
+
+ 
+    if(users[roomName] && users[roomName].length > 0){
+      wsServer.sockets.to(users[roomName][0].id).emit("stream", videoStream);
+    }
+    
+  })
+
+
+  const roomName = 'TEST'
+    
+  socket.on("join_room", () => {
+    console.log('join_room')
     socket.join(roomName);
     
     if(users[roomName]){
@@ -51,52 +71,58 @@ wsServer.on("connection", (socket) => {
     }else{
       users[roomName] = [{id: socket.id}]
     }
- 
+    console.log(users)
     const usersInThisRoom = users[roomName].filter(user => user.id !== socket.id);
     wsServer.sockets.to(socket.id).emit("welcome", usersInThisRoom, socket.id);
 
   });
 
-  socket.on("offer", (offer, offerRecivedId, offerSendId) => {  
+  socket.on("offer", (offer, offerRecivedId, offerSendId) => {
+    console.log('offer')  
     wsServer.sockets.to(offerRecivedId).emit("offer", offer, offerSendId);
   });
   socket.on("answer", (answer, offerSendId, mySocketId) => {
+    console.log('answer')  
+    
     wsServer.sockets.to(offerSendId).emit("answer", answer, mySocketId);
   });
   socket.on("ice", (ice, candidateSendID, candidateReceiveID) => {
+    console.log('ice')
     wsServer.sockets.to(candidateReceiveID).emit("ice", ice, candidateSendID);
   });
+
 
 
   // disconnecting event method 를 사용하는 이유는 
   // 해당 접속 소켓의 정보를 잃어버리기 전이기 떄문이다 
   socket.on("disconnecting", (reason) =>{
     
-   for(const room of socket.rooms){
-      console.log(socket.rooms);
-      var joinRoom = users[room];
-      if (joinRoom !== undefined){
+    for(const room of socket.rooms){
+       console.log(socket.rooms);
+       var joinRoom = users[room];
+       if (joinRoom !== undefined){
+   
+         var idx = joinRoom.findIndex((roomUser) => {
+           return roomUser.id === socket.id;
+         });
+       
+         if(idx > -1){
+           console.log(room);
+           socket.to(room).emit("userDisconnect", socket.id);
+           users[room] = users[room].filter((user) => {
+             return user.id !== socket.id;
+           });
+           console.log("remove :: ", users[room]);
+         
+         }
+     
+       } 
   
-        var idx = joinRoom.findIndex((roomUser) => {
-          return roomUser.id === socket.id;
-        });
-      
-        if(idx > -1){
-          console.log(room);
-          socket.to(room).emit("userDisconnect", socket.id);
-          users[room] = users[room].filter((user) => {
-            return user.id !== socket.id;
-          });
-          console.log("remove :: ", users[room]);
-        
-        }
-    
-      } 
+     }
  
-    }
-
-  })
-});
+   })
+ });
+ 
 
 
 
